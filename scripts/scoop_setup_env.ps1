040d2fa3b1012af75719d97881eb6949bdcf3386
yuzucha16 <yuzucha.endou7@gmail.com>
2024-11-17 22:31:35 +0900

    [script/scoop] add installation app list

--- scripts/scoop_setup_env.ps1
+++ scripts/scoop_setup_env.ps1
@@ -3,6 +3,14 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 Get-ExecutionPolicy
 Set-ExecutionPolicy RemoteSigned # Unrestricted
 
+### scoop installation
+$apps   =@(
+            "teraterm", "winmerge", "rufus", "irfanview", "googlechrome", "brave", "fork", "p4v",
+            "windows-terminal", "pwsh", "nu", "starship", "vim", "notepadplusplus",
+            "PSReadLine", "posh-git", "Terminal-Icons", "scoop-completion",
+            "ghq", "cmake", "gcc", "rustup", "go", "sudo", "which", "less", "openssh", "fzf"
+            )
+
 ### XDG Base Directory setting
 $home_dir   = $env:USERPROFILE + "\.config"
 $config_dir = $env:USERPROFILE + "\.config"
@@ -27,5 +35,16 @@ if( !(Test-Path ~/scoop) ){
   Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 }
 scoop install git
+
+### Install app
+foreach( $item in $apps ){
+  if( Test-Path ~/scoop/apps/$item ){
+    # Already exists
+	Write-Host -ForegroundColor Green [Installed]  $item
+  }else{
+    # starts
+	scoop install $item
+  }
+}
 
 pause
