d7534e611c18c6022d09df178a7aa01ddd5abe48
user <user@example.com>
2025-10-13 22:03:41 +0900

    [free commander] 1st commit

--- _scripts/w2a_copy_dotfiles.bat
+++ _scripts/w2a_copy_dotfiles.bat
@@ -41,7 +41,7 @@ set "FILES[21]=office\Excel.exportedUI|%LOCALAPPDATA%\Microsoft\Office\Excel.off
 set "FILES[22]=office\Normal.dotm|%APPDATA%\Microsoft\Templates\Normal.dotm"
 set "FILES[23]=office\Word.exportedUI|%LOCALAPPDATA%\Microsoft\Office\Word.officeUI"
 set "FILES[24]=obsidian\.obsidian|%MY_ROOT%\templates\notes\.obsidian"
-set "FILES[25]=remember.xml|%XDG_BIN_HOME%\te250907\config\remember.xml"
+set "FILES[25]=freecommander.ini|%USERPROFILE%\scoop\apps\freecommander\current\Settings\freecommander.ini"
 set "MAX_IDX=25
 
 rem set "FILES[17]=obsidian\.obsidian|%MY_ROOT%\templates\notes\.obsidian"
